bf6cef2211679b10ed1ffa081d743f6d330bf03f
paulhmorris <paulh.morris@gmail.com>
2023-02-10 10:36:18 -0600

    Delete unused assets, optimize images

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 import lit from '@astrojs/lit';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,12 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
+  integrations: [
+    sitemap(),
+    mdx(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+    lit(),
+  ], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
