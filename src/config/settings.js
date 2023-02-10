78d32784b51aa589815f3c8a9390bd0c1f1c919a
paulhmorris <paulh.morris@gmail.com>
2023-02-10 11:25:54 -0600

    SEO work

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
   title: `MiNa Designs | A party decoration company`,
   description: `Get the best balloon decorations in Houston, TX. Our balloon arrangements are perfect for birthdays, weddings, corporate events and more. Book today!`,
-  url: `https://mina-designs.onrender.com`, // No trailing slash!
-  name: `MiNa Designs`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  url: `https://mina-designs.onrender.com`,
+  name: `MiNa Designs`,
 };
